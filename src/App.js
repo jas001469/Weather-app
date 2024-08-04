7ef5e2b2352015821d43b57723bb80733ecfea98
Jas Singh Ajmani <164511768+jas001469@users.noreply.github.com>
2024-08-04 15:30:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Weather from './components/weather';
+
+function App() {
+  return (
+    <div className="App">
+     <Weather/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/weather/index.js
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import Search from "../search";
+
+
+export default function Weather(){
+    
+    const [search,setSearch]=useState('');
+    const [loading,setLoding] =useState(false);
+    const [weatherData,setWeatherData]=useState(null);
+
+    async function fetchWeatherData(param) {
+        setLoding(true);
+        try{
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=4f4cbe9119bfae62eea2f3225609d7e3`
+              );
+        
+        const data=await response.json();
+        console.log(data,'data');
+        if(data){
+            setWeatherData(data);
+            setLoding(false);
+        }
+        }catch(e){
+            setLoding(false);
+            console.log(e);
+        }
+        
+    }
+     function handleSearch() {
+
+fetchWeatherData(search);
+    }
+    function getCurrentDate() {
+        return new Date().toLocaleDateString("en-us", {
+          weekday: "long",
+          month: "long",
+          day: "numeric",
+          year: "numeric",
+        });
+      }
+
+    useEffect(() => {
+        fetchWeatherData("bangalore");
+      }, []);
+
+    console.log(loading);
+    return  <div>
+        <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+        />
+        {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div>
+          <div className="city-name">
+            <h2>
+              {weatherData?.name}, <span>{weatherData?.sys?.country}</span>
+            </h2>
+          </div>
+          <div className="date">
+            <span>{getCurrentDate()}</span>
+          </div>
+          <div className="temp">{weatherData?.main?.temp}</div>
+          <p className="description">
+            {weatherData && weatherData.weather && weatherData.weather[0]
+              ? weatherData.weather[0].description
+              : ""}
+          </p>
+          <div className="weather-info">
+            <div className="column">
+              <div>
+                <p className="wind">{weatherData?.wind?.speed}</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+            <div className="column">
+              <div>
+                <p className="humidity">{weatherData?.main?.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+        
+        
+    </div>
+}
